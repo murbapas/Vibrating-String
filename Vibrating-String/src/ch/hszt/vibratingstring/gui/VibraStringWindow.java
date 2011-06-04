17ebcbdaf10ea6b1e28217b5e6b4fc3284c0c074
murbapas <murbapas@bluewin.ch>
2011-06-04 18:58:56 +0200

    precalculating string

--- Vibrating-String/src/ch/hszt/vibratingstring/gui/VibraStringWindow.java
+++ Vibrating-String/src/ch/hszt/vibratingstring/gui/VibraStringWindow.java
@@ -32,229 +32,222 @@ import javax.swing.border.EtchedBorder;
 @SuppressWarnings("serial")
 public class VibraStringWindow extends JFrame {
 
-  /**
-   * The vibrating string
-   */
-  VibraString vibraString;
-  /**
-   * The settings panel
-   */
-  private JPanel settingsP;
-  /**
-   * The panel with the plotted graph
-   */
-  private GraphPlotPanel stringP;
-  /**
-   * Screen resolution
-   */
-  private final Dimension DIM = getDefaultToolkit().getScreenSize();
-  /**
-   * The ratio between screen and windows height respectively
-   */
-  private final double RATIO = 0.95;
-  /**
-   * The calculator thread
-   */
-  private Thread calculatorThread;
-  /**
-   * The graph updater thread
-   */
-  private Thread graphUpdaterThread;
-
-  /**
-   * Creates a new instance of {@code VibraStringWindow}
-   */
-  public VibraStringWindow() {
-    configure();
-    createGUI();
-  }
-
-  /**
-   * Set some basic constraints
-   */
-  private void configure() {
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setResizable(true);
-    this.setSize(DIM.width, (int) (DIM.height * RATIO));
-    this.setTitle("Vibrating String");
-  }
-
-  /**
-   * Creates the GUI
-   */
-  private void createGUI() {
-    setUpPanels();
-  }
-
-  /**
-   * Set up the panels
-   */
-  private void setUpPanels() {
-    Container contentPane = this.getContentPane();
-    settingsP = new JPanel();
-    settingsP.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
-    settingsP.setPreferredSize(new Dimension(this.getSize().width, (int) (this.getSize().height * 0.2)));
-    stringP = new GraphPlotPanel();
-    stringP.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
-
-    IMathFunction f = new TriaFunction();
-    IMathFunction g = new ZeroFunction();
-    
-    vibraString = new VibraString(2, 160.0d, f, g);
-    double precision = 0.1d;
-    int l = (int) (vibraString.getLength() / precision);
-    double[] x = new double[l];
-    double step = 0.0d;
-    for (int i = 0; i < l; i++) {
-      // no other than rounding mode UP or CEILING, otherwise the scaling of 
-      // the panel goes wrong
-      x[i] = new BigDecimal(step).setScale(1, RoundingMode.UP).doubleValue();
-      //System.out.println("muh : "+x[i]);
-      step += precision;
-    }
-    
-    stringP.setX(x);
-    double[] y = f.calc(x);
-    stringP.setY(y);
-
-    contentPane.add(settingsP, BorderLayout.NORTH);
-    contentPane.add(stringP, BorderLayout.CENTER);
-
-    Calculator calculator = new Calculator(x, y, vibraString);
-    GraphUpdater graphUpdater = new GraphUpdater(x, y);
-    calculatorThread = new Thread(calculator);
-    graphUpdaterThread = new Thread(graphUpdater);
-
-    startThreads();
-  }
-
-  /**
-   * A {@code GraphUpdater}.
-   * 
-   * The {@code Runnable} which waits for the newly
-   * calculated y-values
-   *
-   * @author Farhan Fayyaz
-   */
-  public class GraphUpdater implements Runnable {
-
-    /**
-     * The x-values
-     */
-    private double[] x;
-    /**
-     * The y-values
-     */
-    private final double[] y;
-
-    /**
-     * Creates a new instance of {@code GraphUpdater}
-     * @param x the x-values
-     * @param y the y-values
-     */
-    public GraphUpdater(double[] x, double[] y) {
-      this.x = x;
-      this.y = y;
-    }
-
-    @Override
-    public void run() {
-      while (true) {
-        synchronized (y) {
-          try {
-            y.wait();
-          } catch (InterruptedException ex) {
-            ex.printStackTrace();
-          }
-          stringP.setValues(x, y);
-        }
-      }
-    }
-  }
-
-  /**
-   * A {@code Calculator}.
-   * 
-   * The {@code Runnable} which waits for the newly
-   * calculated y-values
-   *
-   * @author Pascal Murbach
-   */
-  public class Calculator implements Runnable {
-
-    /**
-     * The x-values
-     */
-    private double[] x;
-    /**
-     * The y-values
-     */
-    private final double[] y;
     /**
      * The vibrating string
      */
-    private VibraString vibraString;
+    VibraString vibraString;
     /**
-     * The speed constant
+     * The settings panel
      */
-    private double c;
+    private JPanel settingsP;
     /**
-     * The length of the vibrating string
+     * The panel with the plotted graph
      */
-    private double length;
+    private GraphPlotPanel stringP;
     /**
-     * The count of pairs of (x/y)-values
+     * Screen resolution
      */
-    private int pairOfValueCnt;
+    private final Dimension DIM = getDefaultToolkit().getScreenSize();
+    /**
+     * The ratio between screen and windows height respectively
+     */
+    private final double RATIO = 0.95;
+    /**
+     * The calculator thread
+     */
+    private Thread calculatorThread;
+    /**
+     * The graph updater thread
+     */
+    private Thread graphUpdaterThread;
 
     /**
-     * Creates a new instance of {@code VibraStringWindow}.
-     * @param x the x-values
-     * @param y the y-values
-     * @param vibraString the vibrating string
+     * Creates a new instance of {@code VibraStringWindow}
      */
-    public Calculator(double[] x, double[] y, VibraString vibraString) {
-      this.x = x;
-      this.y = y;
-      this.vibraString = vibraString;
-      this.c = vibraString.getC();
-      length = vibraString.getLength();
-      pairOfValueCnt = y.length;
+    public VibraStringWindow() {
+        configure();
+        createGUI();
     }
 
-    public void run() {
-      while (true) {
-        for (double t = 0; t < 1; t += 0.0001) {
-          try {
-            Thread.sleep(20);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
+    /**
+     * Set some basic constraints
+     */
+    private void configure() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.setSize(DIM.width, (int) (DIM.height * RATIO));
+        this.setTitle("Vibrating String");
+    }
 
-          synchronized (y) {
-            for (int i = 0; i < pairOfValueCnt; i++) {
+    /**
+     * Creates the GUI
+     */
+    private void createGUI() {
+        setUpPanels();
+    }
 
-              y[i] = 0.0;
-              for (int n = 1; n < 20; n++) {
-                y[i] += (vibraString.fourierCoeff(n, x[0], x[x.length - 1]) * Math.cos(c * n * Math.PI * t / length)
-                        + vibraString.fourierCoeffST(n) * Math.sin(c * n * Math.PI * t / length))
-                        * Math.sin(n * Math.PI * x[i] / length);
-              }
-            }
-            y.notify();
-          }
+    /**
+     * Set up the panels
+     */
+    private void setUpPanels() {
+        Container contentPane = this.getContentPane();
+        settingsP = new JPanel();
+        settingsP.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
+        settingsP.setPreferredSize(new Dimension(this.getSize().width, (int) (this.getSize().height * 0.2)));
+        stringP = new GraphPlotPanel();
+        stringP.setBorder(new EtchedBorder(EtchedBorder.LOWERED));
+
+        IMathFunction f = new SawToothFunction();
+        IMathFunction g = new ZeroFunction();
+
+        vibraString = new VibraString(2, 250.0d, f, g, 0.1d, 0.0001d, 10);
+
+        double[] x = vibraString.getxGrid();
+        double[] y = vibraString.getyStart();
+
+        stringP.setX(x);
+        stringP.setY(y);
+
+        contentPane.add(settingsP, BorderLayout.NORTH);
+        contentPane.add(stringP, BorderLayout.CENTER);
+
+        Calculator calculator = new Calculator(x, y, vibraString);
+        GraphUpdater graphUpdater = new GraphUpdater(x, y);
+        calculatorThread = new Thread(calculator);
+        graphUpdaterThread = new Thread(graphUpdater);
+
+        startThreads();
+    }
+
+    /**
+     * A {@code GraphUpdater}.
+     *
+     * The {@code Runnable} which waits for the newly
+     * calculated y-values
+     *
+     * @author Farhan Fayyaz
+     */
+    public class GraphUpdater implements Runnable {
+
+        /**
+         * The x-values
+         */
+        private double[] x;
+        /**
+         * The y-values
+         */
+        private final double[] y;
+
+        /**
+         * Creates a new instance of {@code GraphUpdater}
+         * @param x the x-values
+         * @param y the y-values
+         */
+        public GraphUpdater(double[] x, double[] y) {
+            this.x = x;
+            this.y = y;
         }
-      }
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (y) {
+                    try {
+                        y.wait();
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    stringP.setValues(x, y);
+                }
+            }
+        }
     }
-  }
 
-  private void startThreads() {
-    calculatorThread.start();
-    graphUpdaterThread.start();
-  }
+    /**
+     * A {@code Calculator}.
+     *
+     * The {@code Runnable} which waits for the newly
+     * calculated y-values
+     *
+     * @author Pascal Murbach
+     */
+    public class Calculator implements Runnable {
 
-  private void stopThreads() {
-    calculatorThread.interrupt();
-    graphUpdaterThread.interrupt();
-  }
+        /**
+         * The x-values
+         */
+        private double[] x;
+        /**
+         * The y-values
+         */
+        private final double[] y;
+        /**
+         * The vibrating string
+         */
+        private VibraString vibraString;
+        /**
+         * The speed constant
+         */
+        private double c;
+        /**
+         * The length of the vibrating string
+         */
+        private double length;
+        /**
+         * The count of pairs of (x/y)-values
+         */
+        private int pairOfValueCnt;
+
+        private int slices;
+
+        private double[][] yt;
+
+        /**
+         * Creates a new instance of {@code VibraStringWindow}.
+         * @param x the x-values
+         * @param y the y-values
+         * @param vibraString the vibrating string
+         */
+        public Calculator(double[] x, double[] y, VibraString vibraString) {
+            this.x = x;
+            this.y = y;
+            this.vibraString = vibraString;
+            this.c = vibraString.getC();
+            length = vibraString.getLength();
+            pairOfValueCnt = y.length;
+            slices = vibraString.getSlices();
+            yt = vibraString.getYt();
+
+        }
+
+        public void run() {
+            while (true) {
+                for (int t = 0; t < slices; t++) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (y) {
+                        for (int i = 0; i < pairOfValueCnt; i++) {
+
+                            y[i] = yt[t][i];
+                        }
+                        y.notify();
+                    }
+                }
+            }
+        }
+    }
+
+    private void startThreads() {
+        calculatorThread.start();
+        graphUpdaterThread.start();
+    }
+
+    private void stopThreads() {
+        calculatorThread.interrupt();
+        graphUpdaterThread.interrupt();
+    }
 }
--- Vibrating-String/src/ch/hszt/vibratingstring/logic/VibraString.java
+++ Vibrating-String/src/ch/hszt/vibratingstring/logic/VibraString.java
@@ -8,6 +8,8 @@
 package ch.hszt.vibratingstring.logic;
 
 import ch.hszt.vibratingstring.logic.function.IMathFunction;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  *
@@ -31,13 +33,18 @@ public class VibraString {
      * TODO add doc
      */
     private IMathFunction g;
+    private final double EPSILON = 1E-8;
+    private double[] xGrid;
+    private double[] yStart;
+    private double timePrec;
+    private double harmonicComp;
+    private int slices;
 
-    //private double[] x;
+    private double[][] yt;
 
-//    public void setX(double[] x) {
-//        this.x = x;
-//    }
-   
+    public double[][] getYt() {
+        return yt;
+    }
 
     /**
      * Creates a new instance of {@code VibraString}
@@ -46,12 +53,41 @@ public class VibraString {
      * @param f the initial function
      * @param g TODO add doc
      */
-    public VibraString(int length, double c, IMathFunction f, IMathFunction g) {
+    public VibraString(int length, double c, IMathFunction f, IMathFunction g, double precision, double timePrec, int harmonicComp) {
         this.length = length;
         this.c = c;
         this.f = f;
         this.g = g;
-        
+        xGrid = calcXGrid(length, precision);
+        yStart = calcYStart(f, xGrid);
+        this.timePrec = timePrec;
+        this.harmonicComp = harmonicComp;
+        slices = (int) ((2 * length) / (c * timePrec));
+        yt = calcStringMovement();
+    }
+
+    public double getHarmonicComp() {
+        return harmonicComp;
+    }
+
+    public void setHarmonicComp(double harmonicComp) {
+        this.harmonicComp = harmonicComp;
+    }
+
+    public int getSlices() {
+        return slices;
+    }
+
+    public void setSlices(int slices) {
+        this.slices = slices;
+    }
+
+    public double getTimePrec() {
+        return timePrec;
+    }
+
+    public void setTimePrec(double timePrec) {
+        this.timePrec = timePrec;
     }
 
     /**
@@ -110,143 +146,53 @@ public class VibraString {
         this.g = g;
     }
 
-//  /**
-//   * Calculate the set of x-values
-//   * to use in the functions
-//   * TODO parameterize precision
-//   * @return the x-grid
-//   */
-//  public double[] getXGrid() {
-//    double precision = 0.1d;
-//    int l = (int) (length / precision);
-//    double[] x = new double[l];
-//    double step = 0.0d;
-//    for (int i = 0; i < l; i++) {
-//      x[i] = new BigDecimal(step).setScale(1, RoundingMode.UP).doubleValue();
-//      //System.out.println(x[i]);
-//      step += precision;
-//    }
-//    return x;
-//  }
-//  public double fourierCoeff(double a, double b, double[] x, double[] y, double n) {
-//        
-//    int fractions = 20;
-//    double sum = 0.0d;        
-//    double h = (b - a) / fractions;
-//    double[] xi = new double[1];   
-//    
-//    //double sinus = Math.sin(n * Math.PI * x / 10000); 
-//        
-//    //for (int i = 1; i <= fractions - 1; i++) {
-//    for (int i = 1; i <= x.length-1; i++) {
-//      //xi[0] = (a + i * h) * sinus;
-//      xi[0] = (a + i * h) * Math.sin(n*Math.PI*x[i] / length);
-//      sum += f.calc(xi)[0];
-//    }
-//    double sum2 = 0.0d;
-//    //for (int i = 1; i <= fractions; i++) {
-//    for (int i = 1; i <= x.length; i++) {
-//      //xi[0] = (((a + (i - 1) * h) + (a + i * h)) / 2) * sinus;
-//      xi[0] = (((a + (i - 1) * h) + (a + i * h)) / 2) * Math.sin(n*Math.PI*x[i] / length);
-//      sum2 += f.calc(xi)[0];
-//    }
-////    double sf = (h / 3) * (1 / 2 * f.calc(new double[]{a * sinus})[0] + sum + 2 * sum2
-////            + 1 / 2 * f.calc(new double[]{b * sinus})[0]);
-//        double sf = (h / 3) * (1 / 2 * f.calc(new double[]{a * Math.sin()})[0] + sum + 2 * sum2
-//            + 1 / 2 * f.calc(new double[]{b * sinus})[0]);
-//
-//    return 2 / length * sf;
-//  }
+    public double[] getxGrid() {
+        return xGrid;
+    }
+
+    public double[] getyStart() {
+        return yStart;
+    }
+
+    public double[] calcXGrid(int length, double precision) {
+
+        int l = (int) (length / precision);
+        double[] x = new double[l];
+        double step = 0.0d;
+        for (int i = 0; i < l; i++) {
+            // no other than rounding mode UP or CEILING, otherwise the scaling of
+            // the panel goes wrong
+            x[i] = new BigDecimal(step).setScale(1, RoundingMode.UP).doubleValue();
+            //System.out.println("muh : "+x[i]);
+            step += precision;
+        }
+        return x;
+    }
+
+    public double[] calcYStart(IMathFunction f, double[] x) {
+        return f.calc(x, length);
+    }
+
     /**
-     * Calculate the fourier coefficient using simpson's quatrature
+     * Calculate the fourier coefficient using simpson's quadrature
      * method
      * @param n the nth value to solve in the loop
      * @return the coefficient
      */
     public double fourierCoeff(double n, double a, double b) {
 
-        //double bn = (1 / (3 * Math.pow(n, 3) * Math.pow(Math.PI, 3))) * (1 - Math.cos(n * Math.PI));
-//    double bn = (1 / (6 * Math.pow(n, 2) * Math.pow(Math.PI, 2))) * Math.sin(n * Math.PI / 2);
-//    return bn;
+        double h = b - a;
+        double c = (a + b) / 2.0;
+        double d = (a + c) / 2.0;
+        double e = (b + c) / 2.0;
 
-//        double sum = 0.0d;
-//        double a = x[0];
-//        double b = x[x.length - 1];
-//        double h = (b - a) / x.length;
-//        double[] y = f.calc(x);
-//
-//    for (int i = 1; i <= x.length - 1; i++) {
-//      double[] xi = {(a + i * h) * Math.sin(n * Math.PI / x.length)};
-//      sum += f.calc(xi)[0];
-//    }
-//    double sum2 = 0.0d;
-//    for (int i = 1; i <= x.length; i++) {
-//      double[] xi = {(((a + (i - 1) * h) / (a + i * h)) / 2) * Math.sin(n * Math.PI / x.length) };
-//      sum2 += f.calc(xi)[0];
-//    }
-//    double sf = (h / 3) * (1 / 2 * f.calc(new double[]{a})[0] + sum + 2 * sum2
-//            + 1 / 2 * f.calc(new double[]{a})[0]);
-//
-//    return sf;
-
-
-
-//    int l = x.length;
-//    double h = 0.1d;
-//    // double fa = yStart[0] * Math.sin(n*Math.PI*xGrid[0] / length);
-//    //double fb = yStart[l - 1] * Math.sin(n*Math.PI*xGrid[l - 1] / length);
-//    double sum = 0;
-//
-//    for (int i = 1; i < l - 1; i++) {
-//      //sum += yStart[i] * Math.sin(n*Math.PI*xGrid[i] / length);
-//      sum += y[i] * Math.sin(n * Math.PI * x[i] / length);
-//    }
-//    System.out.println("sum: " + sum);
-//
-//    double th = h * sum;
-//    //      System.out.println("bn: " + ((2/length) * th));
-//    //      System.out.println("th: " + th);
-//    //      System.out.println("bn: " + bn);
-//    return (2.0d/length) * th;
-
-
-
-//        double[] y = f.calc(x);
-//        double fbn[] = new double[x.length];
-//        for (int i = 0; i < x.length; i++) {
-//            fbn[i] = (y[i] * Math.sin((n * Math.PI * x[i]) / length));
-//        }
-//
-//
-//        double a = x[0];
-//        double b = x[fbn.length -1];
-//        double h = (b - a) / x.length;
-//       // double sf = (b-a)/6*(fbn[(int)a] + 4*(fbn[(int)((a+b)/2)]+fbn[(int)b]));
-//
-//        double sum = 0.0;
-//        for (int i = 0; i < x.length - 1; i++){
-//            sum += fbn[i];
-//        }
-//
-//        double tf = h * ((fbn[(int)a] + fbn[(int)b])/ 2 + sum );
-//
-//        //return sf;
-//        return (2 / length) * tf;
-
-          double EPSILON = 1E-6;
-//          double a = x[0];
-//          double b = x[x.length -1];
-          double h = b - a;
-          double c = (a + b) / 2.0;
-          double d = (a + c) / 2.0;
-          double e = (b + c) / 2.0;
-           double Q1 = h/6  * (f.calcBn(a, n, length) + 4*f.calcBn(c, n, length) + f.calcBn(b, n, length));
-        double Q2 = h/12 * (f.calcBn(a, n, length) + 4*f.calcBn(d, n, length) + 2*f.calcBn(c, n, length) + 4*f.calcBn(e, n, length) + f.calcBn(b, n, length));
-        if (Math.abs(Q2 - Q1) <= EPSILON)
+        double Q1 = h / 6 * (f.calcBn(a, n, length) + 4 * f.calcBn(c, n, length) + f.calcBn(b, n, length));
+        double Q2 = h / 12 * (f.calcBn(a, n, length) + 4 * f.calcBn(d, n, length) + 2 * f.calcBn(c, n, length) + 4 * f.calcBn(e, n, length) + f.calcBn(b, n, length));
+        if (Math.abs(Q2 - Q1) <= EPSILON) {
             return Q2 + (Q2 - Q1) / 15;
-        else
+        } else {
             return fourierCoeff(n, a, c) + fourierCoeff(n, c, b);
-
+        }
 
 
     }
@@ -258,28 +204,32 @@ public class VibraString {
      */
     public double fourierCoeffST(double n) {
 
-//        int l = xGrid.length;
-//    double[] yStart = g.calc(xGrid);
-//    double h = 0.1;
-//    double fa = yStart[0] * Math.sin(n * Math.PI * xGrid[0] / length);
-//    double fb = yStart[l - 1] * Math.sin(n * Math.PI * xGrid[l - 1] / length);
-//    double sum = 0.0;
-//
-//    for (int i = 1; i < length; i++) {
-//      sum += yStart[i] * Math.sin(n * Math.PI * xGrid[i] / length);
-//    }
-//
-//    double th = h * ((fa + fb) / 2.0d + sum);
-//
-//    return (2.0d / (c * n * Math.PI)) * th;
+
         return 0.0d;
     }
-//  @Override
-//  public String toString() {
-//    StringBuilder sb = new StringBuilder(x.length);
-//    for (int i = 0; i < x.length; i++) {
-//      sb.append(y[i]).append(" ");
-//    }
-//    return sb.toString();
-//  }
+
+    public double[][] calcStringMovement() {
+
+        double[][] yt = new double[slices][yStart.length];
+
+        for (int i = 0; i < yStart.length; i++) {
+            yt[0][i] = yStart[i];
+        }
+
+        for (int t = 1; t < slices; t++) {
+
+
+
+            for (int i = 0; i < yStart.length; i++) {
+
+                for (int n = 1; n < harmonicComp; n++) {
+                    yt[t][i] += (fourierCoeff(n, xGrid[0], xGrid[xGrid.length - 1]) * Math.cos(c * n * Math.PI * (t * timePrec) / length)
+                            + fourierCoeffST(n) * Math.sin(c * n * Math.PI * (t * timePrec) / length))
+                            * Math.sin(n * Math.PI * xGrid[i] / length);
+                }
+            }
+        }
+
+        return yt;
+    }
 }
